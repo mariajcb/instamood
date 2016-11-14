44d8a3bf61020567e5f67ee1e7934bab3dd07290
Lisa Ma <limawebdev@gmail.com>
2016-11-14 16:21:47 -0700

    able to get mood back from Microsoft API

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -6,17 +6,22 @@ app.controller('LoginController', ['$location', '$window', '$scope', 'loginServi
     $location.path('/mood');
   }
   $scope.login = function(){
-    console.log(loginService.getCode());
+    loginService.getCode();
   }
 }])
-app.controller('MoodController', ['$location', '$window', '$scope', 'imgService', function($location, $window, $scope, imgService){
+app.controller('MoodController', ['$location', '$window', '$scope', 'imgService', 'moodService', function($location, $window, $scope, imgService, moodService){
   $scope.vm = {};
   imgService.getImage($window.localStorage['accessToken']).getInstaData().$promise.then(function(data) {
+      $scope.vm.username = data.data.username;
       $scope.vm.src = data.data.profile_picture;
       //put people in database HERE
       var userID = data.data.id;
       imgService.getMedia($window.localStorage['accessToken'],userID).getInstaData().$promise.then(function(data){
-        $scope.vm.imgs = data.data;
+        var images = data.data;
+        moodService.getImage(images[1]).then(function(results){
+          $scope.vm.mood = Object.keys(results[0].scores).reduce(function(a, b){ return results[0].scores[a] > results[0].scores[b] ? a : b });
+        })
+        $scope.vm.img = data.data[0];
       })
      })
 }])
--- public/javascripts/services.js
+++ public/javascripts/services.js
@@ -4,7 +4,7 @@ app.service('loginService', function($location, $resource) {
    var clientID = '71589482a5274c6d843f7577cd3c5b94';
    var igPopup = window.location.replace("https://instagram.com/oauth/authorize/?client_id=" + clientID + "&redirect_uri=" + $location.absUrl().split('#')[0] + "&response_type=token", "igPopup");
    return $location.path;
- }
+  }
  }
 })
 
@@ -12,7 +12,7 @@ app.service('imgService', function($http, $resource) {
  return {
   getImage: function(token) {
    var instaApi = $resource(
-    'https://api.instagram.com/v1/users/self/?access_token='+token, {
+    'https://api.instagram.com/v1/users/self/?access_token=' + token, {
      callback: 'JSON_CALLBACK'
     }, {
      getInstaData: { // call this in the controller
@@ -23,7 +23,7 @@ app.service('imgService', function($http, $resource) {
     });
    return instaApi;
   },
-  getMedia: function(token,userID) {
+  getMedia: function(token, userID) {
    var instaApi = $resource(
     `https://api.instagram.com/v1/users/${userID}/media/recent/?access_token=${token}&count=10`, {
      callback: 'JSON_CALLBACK'
@@ -35,6 +35,27 @@ app.service('imgService', function($http, $resource) {
      }
     });
    return instaApi;
+  }
+ }
+})
+
+app.service('moodService', function($location, $resource, $http) {
+ return {
+  getImage: function(image) {
+    var req = {
+     method: 'POST',
+     url: 'https://api.projectoxford.ai/emotion/v1.0/recognize',
+     headers: {
+      'Content-Type': 'application/json',
+      'Ocp-Apim-Subscription-Key': 'ccbc67bfc9d14545abd87142a0a4d0cd'
+     },
+     data: {
+      'url': image.images.standard_resolution.url
+     }
+    }
+    return $http(req).then(function(data){
+      return data.data;
+    });
   }
  }
 })
