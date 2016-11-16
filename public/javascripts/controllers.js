412f321e4fad94395bc5744788011ab91c501d1a
kelseychapman <kelseyc.chapman@gmail.com>
2016-11-15 18:53:15 -0700

    MAPS ARE PLOTTING OUR DATAAAAAA

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -49,11 +49,11 @@ app.controller('MoodController', ['$window', '$scope', 'imgService', 'moodServic
 }])
 
 //UsrsController handles getting lat and long from our moods database
-app.controller('UsersController', ['$location', '$window', '$scope', 'usersService', function($location, $window, $scope, usersService) {
-    $scope.vm = {};
-    //get users vfrom database
-    usersService.getUsers().then(function(response) {
-        console.log(response);
-        $scope.vm.users = response;
-    })
-}])
+// app.controller('UsersController', ['$location', '$window', '$scope', 'usersService', function($location, $window, $scope, usersService) {
+//     $scope.vm = {};
+//     //get users vfrom database
+//     usersService.getUsers().then(function(response) {
+//         console.log(response);
+//         $scope.vm.users = response;
+//     })
+// }])
