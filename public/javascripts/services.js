096fbdd0f07f8325d542ea251f25565ad8413b34
Lisa Ma <limawebdev@gmail.com>
2016-11-16 10:35:54 -0700

    fixed knex.js file node_env

--- public/javascripts/services.js
+++ public/javascripts/services.js
@@ -111,7 +111,6 @@ app.service('usersService', function($http) {
             return $http.get('/api/users').then(function(response) {
                 // console.log(response.data)
                 return response.data
-
             })
         },
         newUser: function(users) {
