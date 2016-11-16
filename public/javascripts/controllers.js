84c54a6b6d33dc57e89844acf7eafc3f653923e8
kelseychapman <kelseyc.chapman@gmail.com>
2016-11-16 11:44:12 -0700

    geolocation

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -48,16 +48,15 @@ app.controller('MoodController', ['$window', '$scope', 'imgService', 'moodServic
 
 }])
 
-//
-UsrsController handles getting lat and long from our moods database
-app.controller('UsersController', ['$location', '$window', '$scope', 'usersService', function($location, $window, $scope, usersService) {
-    $scope.vm = {};
-    //get users vfrom database
-    usersService.getUsers().then(function(response) {
-        console.log(response);
-        $scope.vm.users = response;
-    })
-}])
+//UsersController handles getting lat and long from our moods database
+// app.controller('UsersController', ['$location', '$window', '$scope', 'usersService', function($location, $window, $scope, usersService) {
+//     $scope.vm = {};
+//     //get users vfrom database
+//     usersService.getUsers().then(function(response) {
+//         console.log(response);
+//         $scope.vm.users = response;
+//     })
+// }])
 
 //
 app.controller('PersonController', ['$window', '$scope', 'imgService', 'personService', function($window, $scope, imgService, personService){
