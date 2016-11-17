79023e1c489b0822ce88214efbb57047b72c497a
mariajcb <bogomazm@gmail.com>
2016-11-16 17:19:35 -0700

    chart working now

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -60,26 +60,64 @@ app.controller('UsersController', ['$location', '$window', '$scope', 'usersServi
 }])
 
 //
-app.controller('PersonController', ['$window', '$scope', 'imgService', 'personService', function($window, $scope, imgService, personService){
-  $scope.vm = {};
-  //get user data from Instagram API
-  imgService.getUser($window.localStorage['accessToken']).getInstaData().$promise.then(function(userData) {
-      $scope.vm.username = userData.data.username;
-      var userID = userData.data.id;
-      //get user media from Instagram API
-      imgService.getMedia($window.localStorage['accessToken'],userID).getInstaData().$promise.then(function(userMedia){
-        var captions = userMedia.data;
-        var text = "";
-        for(var i = 0; i < captions.length; i++){
-          var caption = captions[i].caption.text;
-          text += caption;
-        }
-        //get IBM Watson info
-        var textObj = {text: text}
-        personService.person(textObj)
-        .then(function(response){
-          console.log(`RESPONSE FROM THE CONTROLLER`, response.data[0]);
+app.controller('PersonController', ['$window', '$scope', 'imgService', 'personService', function($window, $scope, imgService, personService) {
+    $scope.vm = {};
+    //get user data from Instagram API
+    imgService.getUser($window.localStorage['accessToken']).getInstaData().$promise.then(function(userData) {
+        $scope.vm.username = userData.data.username;
+        var userID = userData.data.id;
+        //get user media from Instagram API
+        imgService.getMedia($window.localStorage['accessToken'], userID).getInstaData().$promise.then(function(userMedia) {
+            var captions = userMedia.data;
+            var text = "";
+            for (var i = 0; i < captions.length; i++) {
+                var caption = captions[i].caption.text;
+                text += caption;
+            }
+            //get IBM Watson info
+            var textObj = {
+                text: text
+            }
+            personService.person(textObj)
+                .then(function(response) {
+                    console.log(`RESPONSE FROM THE CONTROLLER`, response.data[0]);
+                    $scope.personalityData = response.data[0]
+                    var values = []
+                    var names = []
+                    $scope.personalityData.forEach(item => {
+                       var percent = Math.round((item.percentile * 100))
+                        values.push(percent)
+                        names.push(item.name)
+                    })
+
+                    $scope.myJson = {
+                        "type": "bar",
+                        "plotarea": {
+                            "adjust-layout": true
+                        },
+                        "scale-x": {
+                            "label": { /* Scale Title */
+                                "text": "Here is a category scale",
+                            },
+                            "labels": names /* Scale Labels */
+                        },
+                        "scale-y": {
+                            "label": { /* Scale Title */
+                                "text": "Percentile",
+                            },
+                            "labels": ['0', '10%', '20%', '30%', '40%', '50%', '60%', '70%', '80%', '90%', '100%'] /* Scale Labels */
+                        },
+                        "series": [
+                          {
+                            "values": values
+                          }
+                          // {
+                          //   "values": [21,45,19,11,73]
+                          // }
+                        ]
+                    }
+
+                })
         })
-      })
-     })
+    })
 }])
